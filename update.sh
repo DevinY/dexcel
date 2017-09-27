1e2162ba716da278caf75de1325675261b237479
Devin Yang <devin@ccc.tc>
2017-09-28 01:48:41 +0800

    fix typo

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ docker cp .env dexcel:/var/www/html/.env
 echo "Update is down"
 containerid=`docker-compose ps -q`
 echo "You can commit image as below command:"
-echo "docker commmit ${containerid} deviny/excelify"
+echo "docker commit ${containerid} deviny/excelify:latest"
